902ac349a574ddee5e224728cf7aaf3edf861d1f
Zohar Kapustin <kapustzo@post.bgu.ac.il>
2020-05-03 16:44:23 +0300

    Updates

---
+++ precache-manifest.2be80f8ed923278b5f99ed13f6f6eed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de80b50d7cb4de14749e8b02cc5d37d",
+    "url": "/wix-minesweeper/index.html"
+  },
+  {
+    "revision": "e68c7de36faefe4ec97c",
+    "url": "/wix-minesweeper/static/css/main.ef1f6b03.chunk.css"
+  },
+  {
+    "revision": "fc522b0125dd5b5a6acb",
+    "url": "/wix-minesweeper/static/js/2.48d77e10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wix-minesweeper/static/js/2.48d77e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68c7de36faefe4ec97c",
+    "url": "/wix-minesweeper/static/js/main.3ee89a49.chunk.js"
+  },
+  {
+    "revision": "df54ce6e3d8bd731eee4",
+    "url": "/wix-minesweeper/static/js/runtime-main.e6376b51.js"
+  }
+]);
